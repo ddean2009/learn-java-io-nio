528fa2a02cad3feb8b4c37ef8dc62d12b2444c8a
wayne <wayne@ouba.local>
2022-02-15 16:31:46 +0800

    更新版本到JDK17

---
+++ selector/src/main/java/com/flydean/ChatClient.java
@@ -0,0 +1,55 @@
+package com.flydean;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author wayne
+ * @version ChatClient,  2020/5/22 7:57 下午
+ */
+@Slf4j
+public class ChatClient {
+
+    private static SocketChannel socketChannel;
+    private static ByteBuffer byteBuffer;
+
+    public static void main(String[] args) throws IOException {
+
+        ChatClient chatClient = new ChatClient();
+        String response = chatClient.sendMessage("hello 小师妹！");
+        log.info("response is {}", response);
+        response = chatClient.sendMessage("能不能？");
+        log.info("response is {}", response);
+        chatClient.stop();
+
+    }
+
+    public void stop() throws IOException {
+        socketChannel.close();
+        byteBuffer = null;
+    }
+
+    public ChatClient() throws IOException {
+        socketChannel = SocketChannel.open(new InetSocketAddress("localhost", 9527));
+        byteBuffer = ByteBuffer.allocate(512);
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        byteBuffer = ByteBuffer.wrap(msg.getBytes());
+        String response = null;
+        socketChannel.write(byteBuffer);
+        byteBuffer.clear();
+        socketChannel.read(byteBuffer);
+        byteBuffer.flip();
+        byte[] bytes= new byte[byteBuffer.limit()];
+        byteBuffer.get(bytes);
+        response =new String(bytes).trim();
+        byteBuffer.clear();
+        return response;
+
+    }
+}
---
+++ selector/src/main/java/com/flydean/ChatServer.java
@@ -0,0 +1,74 @@
+package com.flydean;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author wayne
+ * @version ChatServer,  2020/5/22 7:44 下午
+ */
+@Slf4j
+public class ChatServer {
+
+    private static String BYE_BYE="再见";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Selector selector = Selector.open();
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress("localhost", 9527));
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+            while (iter.hasNext()) {
+                SelectionKey selectionKey = iter.next();
+                if (selectionKey.isAcceptable()) {
+                    register(selector, serverSocketChannel);
+                }
+                if (selectionKey.isReadable()) {
+                    serverResonse(byteBuffer, selectionKey);
+                }
+                iter.remove();
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    private static void serverResonse(ByteBuffer byteBuffer, SelectionKey selectionKey)
+            throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        socketChannel.read(byteBuffer);
+        byteBuffer.flip();
+        byte[] bytes= new byte[byteBuffer.limit()];
+        byteBuffer.get(bytes);
+        log.info(new String(bytes).trim());
+        if(new String(bytes).trim().equals(BYE_BYE)){
+            log.info("说再见不如不见！");
+            socketChannel.write(ByteBuffer.wrap("再见".getBytes()));
+            socketChannel.close();
+        }else {
+            socketChannel.write(ByteBuffer.wrap("你是个好人".getBytes()));
+        }
+        byteBuffer.clear();
+    }
+
+    private static void register(Selector selector, ServerSocketChannel serverSocketChannel)
+            throws IOException {
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, SelectionKey.OP_READ);
+    }
+}
